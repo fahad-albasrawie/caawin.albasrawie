faf5b93a99078891964daadb2ed72bd1be69c6bc
Ahmed Khaliif Muumin <109719495+muuminbaargeele@users.noreply.github.com>
2024-04-30 15:28:26 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
-# from flask import Flask
-# from datetime import timedelta
-#
-# app = Flask(__name__)
-#
-# from app.caawin_albasrawie.app.public import accept_help, accept_help_model, ask_help, ask_help_model, submit_feedback_model, submit_feedback
-# from app.caawin_albasrawie.app.admin import dashboard, dashboard_model, login
-#
-# app.config["SECRET_KEY"] = "caawin_albasrawie"
-# # app.permanent_session_lifetime = timedelta(minutes=2)
-# app.permanent_session_lifetime = timedelta(days=365)
-#
-#
+from flask import Flask
+from datetime import timedelta
+
+app = Flask(__name__)
+
+from app.caawin_albasrawie.app.public import accept_help, accept_help_model, ask_help, ask_help_model, submit_feedback_model, submit_feedback
+from app.caawin_albasrawie.app.admin import dashboard, dashboard_model, login
+
+app.config["SECRET_KEY"] = "caawin_albasrawie"
+# app.permanent_session_lifetime = timedelta(minutes=2)
+app.permanent_session_lifetime = timedelta(days=365)
+
+
