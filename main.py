1dfb8d7e5f7680133e007fa1d7b9e6f334c3a03a
unknown <cabbaascadde55@gmail.com>
2024-04-30 16:02:23 +0300

    Muumin is working

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from app import app
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and s
 # settings.
 
+print("Hello Muunin")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=2027)
 
